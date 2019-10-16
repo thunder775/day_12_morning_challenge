42d8f92372fd45b3447fb8c8fbfb4298018cc52a
McLarenCollege <hello@mclarencollege.in>
2019-10-16 08:25:20 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,24 @@
+// Challenge 1
+//Create a function that takes an list of positive and negative numbers.
+// Return a list where the first element is the count of positive numbers and the second element is the sum of negative numbers.
+// Examples
+// countPosSumNeg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]) ➞ [10, -65]
+/// There are a total of 10 positive numbers.
+/// The sum of all negative numbers equals -65.
+
+
+
+// Challenge 2
+// The digit distance between two numbers is the absolute value of the difference of each digit.
+//  To illustrate:
+//
+// digitDistance(234, 489) ➞ 12
+// Since |2 - 4| + |3 - 8| + |4 - 9| = 2 + 5 + 5
+/// Create a function that returns the digit distance between two integers.
+
+
+// Challenge 3
+// Write test cases to test previous functions
+main() {
+
+}
